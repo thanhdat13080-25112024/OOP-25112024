776dcac716f6eb894fe81120651974969ecf5cf8
Thành Đạt <thanhdat13080@gmail.com>
2026-03-16 14:45:45 +0700

    done ex15.1 basic

---
+++ Ex15.1-Basic.py
@@ -0,0 +1,66 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class circle:
+    def __init__(self, center, radius):
+        self.center = center
+        self.radius = radius
+        
+
+class rectangle:
+    def __init__(self, point_1, point_2, point_3, point_4):
+        self.point_1 = point_1
+        self.point_2 = point_2
+        self.point_3 = point_3
+        self.point_4 = point_4
+        
+
+def point_in_circle(circle, point):
+    x = int(input("x: "))
+    y = int(input("y: "))
+    p = Point(x, y)
+    if math.sqrt(math.pow(point.x - circle.center.x) + math.pow(point.y - circle.center.y)) == circle.radius:
+        return True
+    return False
+
+
+def rect_in_circle(circle, rectangle):
+    if math.sqrt(math.pow(rectangle.point_1.x - circle.center.x, 2) + math.pow(rectangle.point_1.y - circle.center.y, 2)) == circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_2.x - circle.center.x, 2) + math.pow(rectangle.point_2.y - circle.center.y, 2)) == circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_3.x - circle.center.x, 2) + math.pow(rectangle.point_3.y - circle.center.y, 2)) == circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_4.x - circle.center.x, 2) + math.pow(rectangle.point_4.y - circle.center.y, 2)) == circle.radius:
+        index += 1
+    if index == 4:
+        return True
+    return False
+
+
+def rect_circle_overlap(circle, rectangle):
+    if math.sqrt(math.pow(rectangle.point_1.x - circle.center.x, 2) + math.pow(rectangle.point_1.y - circle.center.y, 2)) <= circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_2.x - circle.center.x, 2) + math.pow(rectangle.point_2.y - circle.center.y, 2)) <= circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_3.x - circle.center.x, 2) + math.pow(rectangle.point_3.y - circle.center.y, 2)) <= circle.radius:
+        index += 1
+    if math.sqrt(math.pow(rectangle.point_4.x - circle.center.x, 2) + math.pow(rectangle.point_4.y - circle.center.y, 2)) <= circle.radius:
+        index += 1
+    if index >= 1:
+        return True
+    return False
+
+
+center_point = Point(150, 100)
+circle_1 = circle(center_point, 75)
+p1 = Point(100, 50)
+p2 = Point(200, 50)
+p3 = Point(200, 105)
+p4 = Point(100, 150)
+rect = rectangle(p1, p2, p3, p4)
