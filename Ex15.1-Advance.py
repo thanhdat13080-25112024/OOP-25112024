c6ce82e09a70fd018df5e24e2963d0a34c68f9a7
Thành Đạt <thanhdat13080@gmail.com>
2026-03-16 12:24:03 +0700

    done ex15.1 advance

---
+++ Ex15.1-Advance.py
@@ -0,0 +1,41 @@
+class Nhanvien:
+    def __init__(self, tenNhanVien, luongCoBan, heSoLuong):
+        self.__tenNhanVien = tenNhanVien
+        self.__luongCoBan = luongCoBan
+        self.__heSoLuong = heSoLuong
+    LUONG_MAX = 20000000
+    
+    def tinhLuong(self):
+        return self.__luongCoBan * self.__heSoLuong
+    
+    def tangLuong(self, luongTang):
+        luongMoi = self.tinhLuong() + luongTang
+        if luongMoi > Nhanvien.LUONG_MAX:
+            return False
+        self.__luongCoBan += luongTang
+        return True
+    
+    def inTTin(self):
+        print(f"Tên nhân viên: {self.__tenNhanVien}")
+        print(f"Lương cơ bản: {self.__luongCoBan}")
+        print(f"Hệ số lương: {self.__heSoLuong}")
+        print(f"Lương: {self.tinhLuong()}")
+        
+    def getName(self):
+        return self.__tenNhanVien
+    
+    def getSalary(self):
+        return self.__luongCoBan
+    
+    def getSC(self):
+        return self.__heSoLuong
+    
+    
+    def setName(self, name):
+        self.__tenNhanVien = name
+        
+    def setSalary(self, salary):
+        self.__luongCoBan = salary
+    
+    def setSC(self, SC):
+        self.__heSoLuong = SC
