086d9f4824daeef3586441ddbcb0937ee46bd35e
Thành Đạt <thanhdat13080@gmail.com>
2026-04-06 10:58:21 +0700

    Week 5

--- Week_4/Line_Segment.py
+++ Week_4/Line_Segment.py
@@ -1,9 +1,12 @@
 from multipledispatch import dispatch
+import turtle
+import _tkinter
 
 class Point():
     def __init__ (self, x, y):
         self.__x = x
         self.__y = y
-    @dispatch(object, object)
-    def add (self.Point(), other):
-        pass
+
+
+print(turtle.forward(100))
+turtle.done()
